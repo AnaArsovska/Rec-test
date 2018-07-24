280a6e6ea510a5749b8ef7f86fee6e10f3bd913f
Ана Арсовска <anaarsovska@anas-macbook-pro.local>
2018-07-24 16:18:04 -0700

    Initial Commit

---
+++ Rec-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rec-test
+//
+//  Created by Ana Arsovska on 7/24/18.
+//  Copyright © 2018 Ana Arsovska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
